ef4e0c279bace47f5258c0debdd884eb1b1f1ed5
Tim Buschtoens <tbuschto@eclipsesource.com>
2017-10-09 10:25:23 +0200

    Support any number of game buttons in layout

--- src/ui/GameView.ts
+++ src/ui/GameView.ts
@@ -9,20 +9,24 @@ const COLUMNS = 2;
 export default class GameView extends Composite {
 
   private game: Game;
-  private buttons: GameButtonView[];
+  private buttonViews: GameButtonView[];
 
   constructor(properties: CompositeProperties & { game: Game }) {
     super(omit(properties, 'game'));
     this.game = properties.game;
-    this.buttons = reorderButtons(this.game.buttons).map(model => new GameButtonView({ model }));
-    this.append(this.buttons);
+    this.buttonViews = reorderButtons(this.game.buttons).map(model => new GameButtonView({ model }));
+    this.append(this.buttonViews);
     this.on({ resize: this.layoutButtons });
+  }
+
+  private updateButtons() {
+
   }
 
   private layoutButtons = ({ width, height }: WidgetResizeEvent) => {
     let side = Math.min(width, height) / 2;
     let offsetX = (width - side * COLUMNS) / 2;
-    let offsetY = (height - side * Math.ceil(this.buttons.length / COLUMNS)) / 2;
+    let offsetY = (height - side * Math.ceil(this.buttonViews.length / COLUMNS)) / 2;
     this.find(GameButtonView).forEach((button, index) => {
       button.set({
         width: side,
@@ -36,8 +40,12 @@ export default class GameView extends Composite {
 }
 
 function reorderButtons(buttons: GameButtonModel[]) {
-  let rowLength = Math.ceil(buttons.length / COLUMNS);
-  let rowOne = buttons.slice(0, rowLength);
-  let rowTwo = buttons.slice(rowLength).reverse();
-  return rowOne.concat(rowTwo);
+  let result: GameButtonModel[] = [];
+  let rowCount = Math.ceil(buttons.length / COLUMNS);
+  let rowLength = Math.ceil(buttons.length / rowCount);
+  for (let i = 0; i < rowCount; i++) {
+    let row = buttons.slice(i * rowLength, (i + 1) * rowLength);
+    result = result.concat(i % 2 === 0 ? row : row.reverse());
+  }
+  return result;
 }
